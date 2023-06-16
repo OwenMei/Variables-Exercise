25689ff4ab3aaa172b0ba4b33f6dc57650c0e5c4
owen mei <owenzackmei@gmail.com>
2023-06-16 10:20:58 -0400

    init commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Fido";
+            int dogAge = 4;
+            char firstNameOfDog = 'F';
+            bool likesTreats = true;
+            double dogWeight = 4.9;
+            decimal dogMaxSpeed = 16.22m;
+
+            Console.WriteLine($"My dog is called {dogName}, he is {dogAge}." + 
+                $" The first letter of his name is {firstNameOfDog} and it is "+
+                $"{likesTreats} that he likes treats. He has just hit {dogWeight}"+
+                $" pounds today and ran at me at {dogMaxSpeed} mph today");
+
+
         }
     }
 }
